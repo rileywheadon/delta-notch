7f9833c90f866bcf451cf42e601ecd87704317dc
Riley Wheadon <rileywheadon@gmail.com>
2025-03-06 22:19:15 -0800

    First attempt at the stochastic model with the Gillespie algorithm.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Final project for MATH 462 2024W2 with Dr. Eric Cytrynbaum.
 
 By Riley Wheadon, Ziyi Zhuang, and Edwin Huras.
+
+## Creating a Virtual Environment
+
+To run the Gillespie model, you will need to create a Python virtual environment:
+
+1. Open a terminal and run the command `python -m venv .venv`
+2. Then, activate the environment with `source .venv/bin/activate`
+3. Finally, install the necessary packages with `pip install -r requirements.txt`
+
+If you need to add dependencies to the project, install them in the virtual environment using the command `pip install [PACKAGE]`. Then, from the root directory of the project, run `pip freeze > requirements.txt`. This saves your dependencies to the `requirements.txt` file and makes it easier for everyone else to run your code.
---
+++ code/gillespie-model/main.py
@@ -0,0 +1,179 @@
+# IMPORT PYTHON MODULES
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+
+# INITIALIZE PARAMETER VALUES
+KT = 0.0002  # Delta-Notch binding rate
+GE = 0.01    # Extracellular molecule decay rate (used in dN/dt and dD/dt equations)
+GI = 0.05    # NICD decay rate
+NM = 10      # Maximum rate of Notch production
+DM = 10      # Maximum rate of Delta production
+N0 = 100     # Notch hill function (H+) half-max
+D0 = 100     # Delta hill function (H-) half-max
+
+# INITIALIZE HILL FUNCTIONS
+HP = lambda i : (NM * i**2) / ((N0**2) + (i**2))
+HM = lambda i : (DM * D0**2) / ((D0**2) + (i**2))
+
+# INITIALIZE NEIGHBOURS (by index in state, include both directions)
+NEIGHBOURS = np.array([
+    [0, 1],
+    [1, 0],
+])
+
+# INITIALIZE SYSTEM STATE (each element contains integers [N, D, I] for one cell)
+state = np.array([
+    [300, 50, 100],
+    [300, 50, 100],
+])
+
+# INITIALIZE SYSTEM RATES (ri contains rates for reaction i)
+#  - Each pair of neighbours creates one reaction
+#  - Each cell has five other reactions (decay/production)
+rates = np.empty(len(NEIGHBOURS) + (len(state) * 5))
+
+# INITIALIZE SIMULATION TIME
+t = 0
+
+
+# Generates the reaction rates based on the current state
+def update_reaction_rates(state):
+
+    # Initialize empty rate arrays for each reaction
+    r1 = np.empty(len(NEIGHBOURS))
+    r2 = np.empty(len(state))
+    r3 = np.empty(len(state))
+    r4 = np.empty(len(state))
+    r5 = np.empty(len(state))
+    r6 = np.empty(len(state))
+
+    # Iterate over all cells to get the reaction/decay rates
+    for i, cell in enumerate(state):
+
+        # Set all of the decay rates (reactions 2-4)
+        r2[i] = cell[0] * GE  # Rate parameter of Notch decay
+        r3[i] = cell[1] * GE  # Rate parameter of Delta decay
+        r4[i] = cell[2] * GI  # Rate parameter of NICD decay
+
+        # Set all of the production rates (reactions 5-6)
+        r5[i] = HP(cell[0])   # Rate parameter of Notch production
+        r6[i] = HM(cell[1])   # Rate parameter of Delta production
+
+    # Sum over the neighbour pairs to get the binding rates (reaction 1) 
+    for i, (cellA, cellB) in enumerate(NEIGHBOURS):
+        r1[i] = KT * state[cellA][0] * state[cellB][1]
+
+    # Return the combined array of rates
+    return np.concatenate((r1 ,r2, r3, r4, r5, r6))
+
+
+# Takes the reaction rates and generates the next reaction time
+def generate_reaction_time(rates):
+    return np.random.exponential(scale = np.sum(rates))
+
+
+# Generate an event, returning a (reaction, index) tuple
+def generate_event(rates):
+
+    # Sample a random value in [0, 1] and find its index in the partition
+    partition = np.cumsum(rates) / np.sum(rates)
+    event = np.searchsorted(partition, np.random.rand())
+
+    # Check if the event is a binding event
+    if event < len(NEIGHBOURS):
+        return (np.int64(1), event)
+
+    # Otherwise do some modulo fuckery to get the reaction
+    reaction = 2 + ((event - len(NEIGHBOURS)) // len(state))
+    index = (event - len(NEIGHBOURS)) % len(state)
+    return (reaction, index)
+
+
+# Update the state given an event
+def update_state(event):
+    reaction, index = event
+
+    # Binding reaction event
+    if reaction == 1:
+        cellA, cellB = NEIGHBOURS[index]
+        state[cellA][0] -= 1 # Remove Notch from cell A
+        state[cellB][1] -= 1 # Remove Delta from cell B
+        state[cellA][2] += 1 # Add a NICD to cell A
+
+    # Notch decay event
+    if reaction == 2: 
+        state[index][0] -= 1
+
+    # Delta decay event
+    if reaction == 3: 
+        state[index][1] -= 1
+
+    # NICD decay devent
+    if reaction == 4:
+        state[index][2] -= 1
+
+    # Notch production event
+    if reaction == 5: 
+        state[index][0] += 1
+
+    # Delta production event
+    if reaction == 6:
+        state[index][1] += 1
+
+    return state
+    
+
+# Main simulation loop
+def main():
+
+    global state
+    global rates
+    global t
+
+    # Initialize an empty results array
+    steps = 10000
+    vT = np.arange(steps)
+    results = np.empty((steps, 2, 3))
+    for i in vT:
+
+        # Update reaction rates
+        rates = update_reaction_rates(state)
+
+        # Generate a new reaction time and increment t
+        t += generate_reaction_time(rates)
+
+        # Generate an event for this time step
+        event = generate_event(rates)
+
+        # Update the state based on the event
+        state = update_state(event)
+
+        # Add the state to the results array
+        results[i] = state
+
+
+    # Get the trajectories for the notch, delta, and NICD concentrations for each cell
+    N1 = results[:, 0, 0]
+    D1 = results[:, 0, 1]
+    I1 = results[:, 0, 2]
+    N2 = results[:, 1, 0]
+    D2 = results[:, 1, 1]
+    I2 = results[:, 1, 2]
+
+    # Plot the results
+    fig, axs = plt.subplots(3, 1, sharex = True)
+    axs[0].plot(vT, N1)
+    axs[0].plot(vT, N2)
+    axs[0].set_ylabel("NOTCH")
+    axs[1].plot(vT, D1)
+    axs[1].plot(vT, D2)
+    axs[1].set_ylabel("DELTA")
+    axs[2].plot(vT, I1)
+    axs[2].plot(vT, I2)
+    axs[2].set_ylabel("NICD")
+    # plt.show()
+    fig.savefig('fig.pdf')
+
+
+main()
